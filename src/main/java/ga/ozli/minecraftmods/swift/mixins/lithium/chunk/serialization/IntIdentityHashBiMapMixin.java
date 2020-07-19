ec1c7b0ba6b0b7ec524d0d42fd51edb0c5aa0eeb
Oscar Nardone <PaintNinja@users.noreply.github.com>
2020-07-19 13:37:28 +0100

    Minor optimisation to IObjectIntIterable<K>#iterator()

--- src/main/java/ga/ozli/minecraftmods/swift/mixins/lithium/chunk/serialization/IntIdentityHashBiMapMixin.java
+++ src/main/java/ga/ozli/minecraftmods/swift/mixins/lithium/chunk/serialization/IntIdentityHashBiMapMixin.java
@@ -1,14 +1,19 @@
 package ga.ozli.minecraftmods.swift.mixins.lithium.chunk.serialization;
 
+import com.google.common.collect.Iterators;
+import net.minecraft.util.IObjectIntIterable;
 import net.minecraft.util.IntIdentityHashBiMap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Iterator;
+import java.util.Objects;
 
 @Mixin(IntIdentityHashBiMap.class)
-public class IntIdentityHashBiMapMixin<K> {
+public class IntIdentityHashBiMapMixin<K> implements IObjectIntIterable<K> {
     @Shadow
     private K[] byId;
 
@@ -21,4 +26,15 @@ public class IntIdentityHashBiMapMixin<K> {
     public K getByValue(int value) {
         return value >= 0 && value < this.byId.length ? this.byId[value] : null;
     }
+
+    /**
+     * @reason Replaced Guava's functional primitive with Java API, remove Lambda
+     * @author Paint_Ninja
+     */
+    @Overwrite
+    @Nonnull
+    public Iterator<K> iterator() {
+        return Iterators.filter(Iterators.forArray(this.byId), Objects::nonNull);
+    }
+
 }
